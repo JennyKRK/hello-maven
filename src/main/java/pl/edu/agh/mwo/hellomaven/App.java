465230d804c50313f4b5ddb1f6542c6e6eb2e388
JoannaN <joannan@student.agh.edu.pl>
2023-03-05 10:33:03 +0100

    log

--- src/main/java/pl/edu/agh/mwo/hellomaven/App.java
+++ src/main/java/pl/edu/agh/mwo/hellomaven/App.java
@@ -1,5 +1,11 @@
 package pl.edu.agh.mwo.hellomaven;
 
+import com.indvd00m.ascii.render.Render;
+import com.indvd00m.ascii.render.api.ICanvas;
+import com.indvd00m.ascii.render.api.IContextBuilder;
+import com.indvd00m.ascii.render.api.IRender;
+import com.indvd00m.ascii.render.elements.PseudoText;
+
 /**
  * Simple main application
  */
@@ -7,11 +13,22 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        render();
     }
     
     public int getRandomNumber() {
         // chosen by fair dice roll.
         // guaranteed to be random
         return 4;
+    }
+
+    public static void render(){
+        IRender render = new Render();
+        IContextBuilder builder = render.newBuilder();
+        builder.width(120).height(20);
+        builder.element(new PseudoText("Hello maven"));
+        ICanvas canvas = render.render(builder.build());
+        String s = canvas.getText();
+        System.out.println(s);
     }
 }
